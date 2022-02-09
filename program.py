c4c63b9a5a2a067034279d5bd0b67c1c453d4b19
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2022-02-09 15:03:41 +0800

    reading json file

--- program.py
+++ program.py
@@ -5,3 +5,9 @@ from fpdf import FPDF
 #creating pdf file
 pdf = FPDF ('P', 'cm', 'A4')
 pdf.add_page()
+
+#reading json file
+resume_details = open('details.json', 'r')
+details = resume_details.read
+information = json.loads(details)
+
