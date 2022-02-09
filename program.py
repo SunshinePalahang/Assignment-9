eb51967fca6aada147e0dde182b2dd1e83bce96c
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2022-02-09 16:13:54 +0800

    using for loop for reading informations

--- program.py
+++ program.py
@@ -8,6 +8,10 @@ pdf.add_page()
 
 #reading json file
 resume_details = open('details.json', 'r')
-details = resume_details.read
+details = resume_details.read()
 information = json.loads(details)
 
+#for looping informations
+for i in information:
+    pdf.set_font('times','B',18)
+    pdf.cell(10, 1, f"{i['name']}", align="C", ln= 1),
